8ff25e5780178c5f3f8b79e3afd4fbdd56612ece
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-17 17:18:45 +0200

    events-01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ClickCounter } from "./ClickCounter";
 import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
@@ -9,7 +10,12 @@ export class App extends React.Component {
       <div>
         <Hello />
         <Welcome name="John" age={28} />
-        <Counter initialValue={0} incrementAmount={1} incrementInterval={1000}/>
+        <Counter
+          initialValue={0}
+          incrementAmount={1}
+          incrementInterval={1000}
+        />
+        <ClickCounter />
       </div>
     );
   }
