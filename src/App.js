dd6b0b4bf068e285dbcb5a88442c51f3b2915f74
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-17 15:51:33 +0200

    conditional rendering-02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
-import { Age } from "./Age";
 
 export class App extends React.Component {
   render() {
