cc8dc15d7722e74e4b84517b71c412d6615b2cf3
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-17 16:42:58 +0200

    components-lifecycle-01

--- src/Counter.js
+++ src/Counter.js
@@ -5,8 +5,7 @@ export class Counter extends React.Component {
   state = {
     count: this.props.initialValue,
   };
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     setInterval(() => {
       this.setState({ count: this.state.count + this.props.incrementAmount });
     }, this.props.incrementInterval);
