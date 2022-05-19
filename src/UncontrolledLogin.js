a91e4028fd9b68d77e6a3e6570330e6db0f91cf8
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-19 14:56:31 +0200

    forms-06

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { createRef } from "react";
 
 export class UncontrolledLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.userInputRef = createRef();
+  }
   handleFormSubmit = (event) => {
     event.preventDefault();
     const username = event.target.elements.username.value;
@@ -9,11 +14,14 @@ export class UncontrolledLogin extends React.Component {
     console.log({ username, password, remember });
   };
 
+  componentDidMount() {
+    this.userInputRef.current.focus();
+  }
   render() {
     return (
       <div>
         <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
-          <input name="username"></input>
+          <input name="username" ref={this.userInputRef}></input>
           <input name="password" type="password"></input>
           <input name="remember" type="checkbox"></input>
           <button type="submit">uncontrolled login</button>
