003be2723b17db40e16e27762168edd30a59ec3c
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-24 18:48:34 +0200

    function-components-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,21 +1,11 @@
 import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component {
-  render() {
-    return (
-      <div className="welcome">
-        <p>Welcome {this.props.name}!</p>
-        {this.props.age > 18 ? (
-          <Age age={this.props.age} />
-        ) : (
-          <p>You are very young!</p>
-        )}
-      </div>
-    );
-  }
+export function Welcome(props) {
+  return (
+    <div className="welcome">
+      <p>Welcome {props.name}!</p>
+      {props.age > 18 ? <Age age={props.age} /> : <p>You are very young!</p>}
+    </div>
+  );
 }
-
-Welcome.defaultProps = {
-  name: "nico",
-};
