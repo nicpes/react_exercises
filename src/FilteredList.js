2b3dcc9ba13d1f339f3a257e9acfb22bdb35b5bd
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-28 00:19:22 +0200

    useMemo

--- src/FilteredList.js
+++ src/FilteredList.js
@@ -1,24 +1,13 @@
-import React, { memo, useMemo, useState } from "react";
-const array = [
-  { id: 1, age: 15, name: "Bill" },
-  { id: 2, age: 22, name: "Nick" },
-  { id: 3, age: 28, name: "Marc" },
-  { id: 4, age: 12, name: "Luke" },
-];
+import React, { useMemo } from "react";
+
 export function FilteredList() {
-  const [list, setList] = useState();
-
-  const memoArray = useMemo(
-    () =>
-      array.filter((user) => {
-        return user.age > 18;
-      }),
-    []
-  );
-
-  if (memoArray == true) {
-    setList(memoArray);
-  }
+  const array = [
+    { id: 1, age: 15, name: "Bill" },
+    { id: 2, age: 22, name: "Nick" },
+    { id: 3, age: 28, name: "Marc" },
+    { id: 4, age: 12, name: "Luke" },
+  ];
+  const memoArray = useMemo(() => array.filter((user) => user.age > 18), [array]);
 
   return (
     <div>
