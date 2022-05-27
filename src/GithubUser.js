02c3f1988052ad69d7b46f7f260c66c8ee5a317d
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-27 11:18:32 +0200

    useEffect-04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -3,13 +3,21 @@ import React, { useEffect, useState } from "react";
 export function Githubuser({ name }) {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(`https://api.github.com/users/${name}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        setData(json);
-      });
+    {
+      name &&
+        fetch(`https://api.github.com/users/${name}`)
+          .then((response) => {
+            return response.json();
+          })
+          .then((json) => {
+            setData(json);
+          });
+    }
   }, [name]);
-  return <div>{data && <h1>{data.name}</h1>}</div>;
+  return (
+    <div>
+      <p>GITHUB USER</p>
+      {data && <h1>{data.name}</h1>}
+    </div>
+  );
 }
