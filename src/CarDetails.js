2da48814bc9a5b095a29faa254925c2eb7b25c97
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-29 11:00:06 +0200

    useRef-02

--- src/CarDetails.js
+++ src/CarDetails.js
@@ -1,46 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useRef } from "react";
-import { useState } from "react";
 
 export function CarDetails({ initialData }) {
-  const modelRef = useRef();
-  const dataRef = useRef();
-  const colorRef = useRef();
-
-  function handleInputModel(event) {
-    event.preventDefault();
-    modelRef.current.value = event.target.value;
-  }
-  function handleInputYear(event) {
-    event.preventDefault();
-    dataRef.current.value = event.target.value;
-  }
-  function handleInputColor(event) {
-    event.preventDefault();
-    colorRef.current.value = event.target.value;
-  }
+  const modelRef = useRef(initialData.model);
+  const dataRef = useRef(initialData.year);
+  const colorRef = useRef(initialData.color);
 
   return (
     <div>
       <form>
-        <input
-          onChange={handleInputModel}
-          ref={modelRef}
-          defaultValue={initialData.model}
-          name="model"
-        />
-        <input
-          onChange={handleInputYear}
-          ref={dataRef}
-          defaultValue={initialData.year}
-          name="year"
-        />
-        <input
-          onChange={handleInputColor}
-          ref={colorRef}
-          defaultValue={initialData.color}
-          name="color"
-        />
+        <input ref={modelRef} defaultValue={modelRef.current} name="model" />
+        <input ref={dataRef} defaultValue={dataRef.current} name="year" />
+        <input ref={colorRef} defaultValue={colorRef.current} name="color" />
       </form>
     </div>
   );
