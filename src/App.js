364e8996d87a5cfdfeb27d64f809e94ab0cc91bf
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-30 09:44:31 +0200

    react-router-02

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { ClickCounter } from "./ClickCounter";
 
 export function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Welcome name="nico" />} />
+        <Route path="click-counter" element={<ClickCounter />} />
       </Routes>
     </div>
   );
