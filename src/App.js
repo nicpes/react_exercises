d4a7e9a87ea62f6079d3ead76e06bd5fb8f4e539
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-31 16:47:09 +0200

    router-06

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { ClickCounter } from "./ClickCounter";
 import { Githubuser } from "./GithubUser";
+import { GithubUserList } from "./GithubUserList";
+import { ShowGithubUser } from "./ShowGithubUser";
+import { GithubUser2 } from "./GithubUser2";
 
 export function App() {
   return (
     <div>
+      
       <Routes>
         <Route path="/" element={<Welcome name="nico" />} />
         <Route path="click-counter" element={<ClickCounter />} />
-        <Route path="users/:name" element={<Githubuser />} />
-        <Route path="*" element={<div><p>Not Found</p></div>} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <p>Not Found</p>
+            </div>
+          }
+        />
+        <Route path="users" element={<GithubUserList />}>
+          <Route path=":name" element={<ShowGithubUser />} />
+        </Route>
       </Routes>
       <Link to="/">Home</Link> <br />
       <Link to="/click-counter">Click Counter</Link> <br />
-      <Link to="/users/nicpes">GitHub User</Link>
+      <Link to="/users">GitHubUserList</Link>
     </div>
   );
 }
