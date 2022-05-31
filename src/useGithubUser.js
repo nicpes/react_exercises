0ff9b4eba96f3db282845c932ecd4c4ff1b6d213
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-31 17:16:36 +0200

    swr-01

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,32 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useSWR } from "swr";
 
-export function useGithubUser({name}) {
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    setLoading(true);
-    {
-        fetch(`https://api.github.com/users/${name}`)
-          .then((response) => {
-            if (response.status !== 200) {
-              setError(new Error("User not found"));
-            }
-            return response.json();
-          })
-          .then((json) => {
-            setLoading(false);
-            setData(json);
-          })
-          .catch((error) => {
-            setError(error);
-          });
-        }
-  }, [name]);
+export function useGithubUser({ name }) {
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${name}`,
+    fetcher
+  );
 
   return {
     data,
