b2e2a6740bf132644f591d1c472dc2a273da928e
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-05-31 18:02:31 +0200

    swr-02

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useSWR } from "swr";
+import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function useGithubUser({ name }) {
-  const { data, error } = useSWR(
-    `https://api.github.com/users/${name}`,
+  const { data, error, loading } = useSWR(
+    () => (name ? `https://api.github.com/users/${name}` : null),
     fetcher
   );
 
