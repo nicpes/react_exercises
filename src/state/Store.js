af768b35ca9e53d5d8382f80a9d0fa377e5a8e7e
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-06-02 14:46:48 +0200

    redux-05

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import { counterReducer, incrementCounter } from "./CounterReducer";
-
+import { todosReducer, addTodo, removeTodo, editTodo } from "./TodosReducer";
+import { combineReducers } from "redux";
 const rootReducer = combineReducers({
   counter: counterReducer,
   todos: todosReducer,
